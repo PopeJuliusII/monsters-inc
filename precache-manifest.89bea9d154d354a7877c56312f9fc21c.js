6ca58b05913f1d6e78ce3fe0db6bffea2f9b2951
PopeJuliusII <edgar@Pactolus.local>
2020-03-31 13:01:42 -0400

    Updates

---
+++ precache-manifest.89bea9d154d354a7877c56312f9fc21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b738bca0f2bde3038942fa62c8e24d5d",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "3e5c22f19e18059955ff",
+    "url": "/monsters-inc/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "d4759df7932d468bd579",
+    "url": "/monsters-inc/static/js/2.9778ab63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-inc/static/js/2.9778ab63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5c22f19e18059955ff",
+    "url": "/monsters-inc/static/js/main.5ad3992f.chunk.js"
+  },
+  {
+    "revision": "07a090df8fad534e38da",
+    "url": "/monsters-inc/static/js/runtime-main.28a4af69.js"
+  }
+]);
